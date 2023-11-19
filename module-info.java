bdd2ec6359650a72ec945ef5193227e61eee57d9
ShivamPandey3639 <133558433+ShivamPandey3639@users.noreply.github.com>
2023-11-19 19:39:31 +0530

    Delete module-info.java

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.stickyhero {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.stickyhero to javafx.fxml;
-    exports com.example.stickyhero;
-}
